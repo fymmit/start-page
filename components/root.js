5877f2d4faf200bdf40060bd3aa44ac13444da95
fymmit <blomqvist.timo@gmail.com>
2019-08-25 19:28:48 +0300

    Hide WeatherFrame, it's bad

--- components/root.js
+++ components/root.js
@@ -6,10 +6,10 @@ export const Root = () => {
     return (
         e('div', { className: 'flex-col jc-sb min-h-100p' }, [
             e(LinkContainerList, { key: 'link-list' }, null),
-            e(WeatherFrame, {
-                key: 'weather',
-                city: 'turku',
-            }, null)
+            // e(WeatherFrame, {
+            //     key: 'weather',
+            //     city: 'turku',
+            // }, null)
         ])
     );
 };
